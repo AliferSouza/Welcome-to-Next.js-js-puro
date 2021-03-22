46f99501e6d9828440756809183d2ff8ffdc5d2f
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2021-03-21 22:20:06 -0300

    Add files via upload

---
+++ Next.js/src/Index.js
@@ -0,0 +1,67 @@
+
+
+const Index = {
+  render() {
+    document.body.innerHTML = `
+    <div class="container">
+      <header>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        </header>
+
+      <main class="main">
+        <h1 class="title">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p class="description">
+      
+          <code class="code">pages/index.js</code>
+        </p>
+
+        <div class="grid">
+          <a href="https://nextjs.org/docs" class="card">
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" class="card">
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            class="card"
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            class="card"
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer class="footer">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>`
+  }
+}
+
+  export { Index };
